13bcce2c30d3bf3cb64f2c9d9b7648c473448f7e
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-09-28 20:51:08 -0600

    threading + initial loop

--- src/com/cvs0/cvsengine2d/GameContainer.java
+++ src/com/cvs0/cvsengine2d/GameContainer.java
@@ -1,12 +1,18 @@
 package com.cvs0.cvsengine2d;
 
 public class GameContainer implements Runnable {
+	
+	private Thread thread;
+	private boolean running = false;
+	private static final double UPDATE_CAP = 1.0 / 60.0;
+	
 	public GameContainer() {
 		
 	}
 	
 	public void start() {
-		
+		thread = new Thread(this);
+		thread.run();
 	}
 	
 	public void stop() {
@@ -14,7 +20,16 @@ public class GameContainer implements Runnable {
 	}
 	
 	public void run() {
+		running = true;
 		
+		double firstTime = 0;
+		double lastTime = 0;
+		double passedTime = 0;
+		double unprocessedTime = 0;
+		
+		while(running) {
+			
+		}
 	}
 	
 	private void dispose() {
